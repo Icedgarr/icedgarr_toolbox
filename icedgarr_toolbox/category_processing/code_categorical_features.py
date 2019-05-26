bf6727f0974b270b4a2e2825f7436370f03d2d7c
Icedgarr <garriga77@gmail.com>
2019-05-26 22:59:07 +0200

    :sparkles: Create category coding method.

---
+++ icedgarr_toolbox/category_processing/code_categorical_features.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from pandas import DataFrame
+
+
+def code_categorical_features(categorical_columns: List[str], dataframe: DataFrame) -> DataFrame:
+    dataframe[categorical_columns] = dataframe[categorical_columns].astype('category')
+    category_names = {}
+    for category in categorical_columns:
+        mylist = []
+        subcategory_names = {'null':-1}
+        for cat, index in enumerate(dataframe[category].cat.categories):
+            subcategory_names[index] =  cat
+        mylist.append(subcategory_names)
+        category_names[category] = mylist
+        dataframe[category] = dataframe[category].cat.codes
+    return dataframe
