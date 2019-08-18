239a74f9223714bea70299305d1f6856f4ce5f1c
Icedgarr <garriga77@gmail.com>
2019-08-18 22:22:15 +0200

    Rename categorize_train_and_test and add it to init.

--- icedgarr_toolbox/category_processing/__init__.py
+++ icedgarr_toolbox/category_processing/__init__.py
@@ -1,1 +1,2 @@
 from icedgarr_toolbox.category_processing.code_categorical_features import code_categorical_features
+from icedgarr_toolbox.category_processing.categorize_train_and_test import categorize_train_and_test
